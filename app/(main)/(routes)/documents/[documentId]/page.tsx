afee8ff9a03c38ec176f3e2475e363ebd0d06834
Michael Frieze <michaelfrieze@icloud.com>
2023-10-21 18:20:30 -0400

    Cover image and file upload

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { Id } from "@/convex/_generated/dataModel";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
+
 import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
 import { Toolbar } from "@/components/toolbar";
+import { Cover } from "@/components/cover";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface DocumentIdPageProps {
   params: {
@@ -17,9 +19,19 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
     documentId: params.documentId,
   });
 
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content,
+    });
+  };
+
   if (document === undefined) {
     return (
       <div>
+        <Cover.Skeleton />
         <div className="mx-auto mt-10 md:max-w-3xl lg:max-w-4xl">
           <div className="space-y-4 pl-8 pt-4">
             <Skeleton className="h-14 w-[50%]" />
@@ -38,6 +50,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
   return (
     <div className="pb-40">
+      <Cover url={document.coverImage} />
       <div className="mx-auto md:max-w-3xl lg:max-w-4xl">
         <Toolbar initialData={document} />
       </div>
